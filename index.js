d5200313dacede2cd39cf1eeba93bbccfb6934af
Adriana McGee <adriana@Adrianas-MBP.lan>
2022-06-07 09:53:11 -0400

    commit tree 00634e55b5177b8c6a3d7a5b18af3f3fade309c5

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideHeight)
+{ 
+  let sample;
+  if (rideHeight <= 400){
+    sample = 'This one is on me!'
+    return sample;
+  } else {
+    if (rideHeight <= 2000 || rideHeight >= 401){
+      sample = 'That will be twenty bucks.';
+      return sample;
+    } else {
+      if (rideHeight > 2001){
+        sample = 'I will gladly take your thirty bucks.';
+        return sample;
+      }
+    }
+  }
+  scuberGreetingForFeet(2002);
   // Write your code here!
 }
 
